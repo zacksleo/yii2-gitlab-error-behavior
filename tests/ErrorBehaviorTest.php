bd87f6d210b74ba34a8cb425fa76934be6dc41c0
陈家辉 <chenjiahui@haolianluo.com>
2017-08-22 17:19:26 +0800

    test(testing): 搭建测试环境

---
+++ tests/ErrorBehaviorTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: graychen
+ * Date: 2017/8/11
+ * Time: 上午11:15
+ */
+namespace zacksleo\yii2\gitlab\behaviors\tests;
+
+use zacksleo\yii2\gitlab\behaviors\tests\TestCase as TestCase;
+use zacksleo\yii2\gitlab\behaviors\ErrorBehavior;
+use Yii;
+
+class ErrorBehaviorTest extends TestCase
+{
+    public function testBeforeAction()
+    {
+
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,69 @@
+<?php
+namespace zacksleo\yii2\gitlab\behaviors\tests;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+use Faker\Factory;
+
+/**
+ * This is the base class for all yii framework unit tests.
+ */
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+    protected $model;
+    protected $faker;
+    protected $generatestring;
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->mockWebApplication();
+        $this->faker=Factory::create('zh_CN');     //伪数据生成器
+    }
+    protected function tearDown()
+    {
+        $this->destroyApplication();
+        unset($this->model);
+    }
+
+    protected function mockWebApplication($config = [], $appClass = '\yii\web\Application')
+    {
+       return new $appClass(ArrayHelper::merge([
+            'id' => 'testapp',
+            'basePath' => __DIR__,
+            'vendorPath' => $this->getVendorPath(),
+            'components' => [
+                'db' => [
+                    'class' => 'yii\db\Connection',
+                    'dsn' => 'mysql:host=localhost:3306;dbname=test',
+                    'username'=> 'root',
+                    'password'=> '206065',
+                    'tablePrefix' => 'tb_'
+                ],
+                'i18n' => [
+                    'translations' => [
+                        '*' => [
+                            'class' => 'yii\i18n\PhpMessageSource',
+                        ]
+                    ]
+                ],
+            ]
+        ], $config));
+    }
+    /**
+     * @return string vendor path
+     */
+    protected function getVendorPath()
+    {
+        return dirname(__DIR__) . '/vendor';
+    }
+    /**
+     * Destroys application in Yii::$app by setting it to null.
+     */
+    protected function destroyApplication()
+    {
+        if (\Yii::$app && \Yii::$app->has('session', true)) {
+            \Yii::$app->session->close();
+        }
+        \Yii::$app = null;
+    }
+}
